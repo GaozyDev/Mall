98bf065a0ef2a67dcb55d585c1208b6a89c8118d
GaozyDev <670453367@qq.com>
2022-06-23 09:02:47 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,26 @@
 </template>
 
 <script>
-
+import storage from "@/storage";
 
 export default {
   name: 'App',
-  components: {
+  data() {
+    return {
 
+    }
+  },
+  components: {},
+  mounted() {
+    // storage.setItem('a',1)
+    // storage.setItem('user', {a:1})
+    // storage.setItem('abc', {a:1},'user')
+    // storage.clear('a')
+    // storage.clear('a','user')
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
