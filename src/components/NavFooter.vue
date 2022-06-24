e7bf5921c1f5c85626f9e31288e50e2cffdff401
GaozyDev <670453367@qq.com>
2022-06-24 13:47:00 +0800

    nav-footer组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="../../public/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="https://www.baidu.com/" target="_blank">LqGitHub主页</a><span>|</span>
+      <a href="https://www.baidu.com/" target="_blank">LqGitHub主页</a><span>|</span>
+      <a href="https://www.baidu.com/" target="_blank">LqGitHub主页</a><span>|</span>
+      <a href="https://www.baidu.com/" target="_blank">LqGitHub主页</a>
+    </div>
+    <div class="copyright">版权 <span class="domain">https://www.baidu.com/</span></div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +19,35 @@ export default {
 }
 </script>
 <style lang="scss">
-
+  .footer{
+    height: 234px;
+    border-top: 4px solid #FF6600;
+    background-color: #333333;
+    color: #999999;
+    font-size: 16px;
+    text-align: center;
+    .footer-logo{
+      margin-top: 46px;
+      margin-bottom: 31px;
+      img{
+        width: 53px;
+        height: 36px;
+        margin-bottom: 13px;
+      }
+    }
+    .footer-link{
+      a{
+        color: #999999;
+      }
+      span{
+        margin: 0 10px;
+      }
+      margin-bottom: 13px;
+    }
+    .copyright{
+      .domain{
+        color: #FF6600;
+      }
+    }
+  }
 </style>
