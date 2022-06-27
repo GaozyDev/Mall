a6ab59dbfbeebd24206d3e98aa5dd56009b40007
GaozyDev <670453367@qq.com>
2022-06-27 10:19:44 +0800

    轮播图菜单

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,16 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="children"></div>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" :key="i">
+                  <li v-for="(sub,j) in item" :key="j">
+                    <a :href="sub?'/#/product/' +sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="">
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
@@ -107,6 +116,31 @@ export default {
           id: '',
           img: '/imgs/slider/slide-5.jpg'
         }
+      ],
+      menuList:[
+          [
+            {
+              id:30,
+              img:'/imgs/item-box-1.png',
+              name:'小米CC9'
+            },
+            {
+              id:31,
+              img:'/imgs/item-box-2.png',
+              name:'小米青春版'
+            },
+            {
+              id:32,
+              img:'/imgs/item-box-3.jpg',
+              name:'Redemi K20 Pro'
+            },
+            {
+              id:33,
+              img:'/imgs/item-box-4.jpg',
+              name:'移动4G专区'
+            }
+          ],
+        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
       ]
     }
   }
@@ -115,47 +149,78 @@ export default {
 <style lang="scss">
 @import "src/assets/scss/mixin.scss";
 .index {
-  .container {
+  .swiper-box {
     .swiper-container {
       height: 451px;
       .swiper-button-prev{
         left: 274px;
       }
+      img {
+        width: 100%;
+        height: 100%;
+      }
     }
-
-    img {
-      width: 100%;
-      height: 100%;
-    }
-    .swiper-box{
-      .nav-menu{
-        position: absolute;
-        width: 264px;
-        height: 451px;
-        z-index: 9;
-        padding: 26px 0;
-        box-sizing: border-box;
-        background-color: #55585a7a;
-        .menu-wrap{
-          .menu-item{
-            height: 50px;
-            line-height: 50px;
-            a{
-              position: relative;
-              display: block;
-              font-size: 16px;
-              color: #ffffff;
-              padding-left: 30px;
-              &:after{
-                position: absolute;
-                right: 30px;
-                top: 17.5px;
-                content: ' ';
-                @include bgImg(10px,15px,'../../public/imgs/icon-arrow.png');
-              }
+    .nav-menu{
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      box-sizing: border-box;
+      background-color: #55585a7a;
+      .menu-wrap{
+        .menu-item{
+          height: 50px;
+          line-height: 50px;
+          a{
+            position: relative;
+            display: block;
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            &:after{
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: ' ';
+              @include bgImg(10px,15px,'../../public/imgs/icon-arrow.png');
             }
-            &:hover{
-              background-color: #FF6600;
+          }
+          &:hover{
+            background-color: #FF6600;
+            .children{
+              display: block;
+            }
+          }
+          .children{
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: #ffffff;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid #e5e5e5;
+            ul{
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              a{
+                color: #333333;
+                font-size: 14px;
+              }
+              img{
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
             }
           }
         }
