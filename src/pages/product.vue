8645e1b4d953d526c3af126cba5654da7f4fae0a
GaozyDev <670453367@qq.com>
2022-06-29 10:47:59 +0800

    产品站

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <ProductParam>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </ProductParam>
+
   </div>
 </template>
 <script>
-
+import ProductParam from "@/components/ProductParam";
 export default {
   name:'product',
+  components:{ProductParam}
 }
 </script>
+<style lang="scss">
+.product{
+  button{
+    margin-left: 10px;
+  }
+}
+</style>
